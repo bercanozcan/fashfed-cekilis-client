07b8aab80f59832d907fee81f230123cac926f65
Bercan Ozcan <bercanozcan@gmail.com>
2022-05-09 15:19:17 +0300

    url change

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,7 @@ export default {
   ],
 
   axios: {
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'http://fashfed.bercanozcan.com/api/',
     headers: {
 			accept: 'application/json',
 		},
